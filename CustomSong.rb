535b0baae28860c5f05d6cceeec2832e4048899f
cutebananaq <160057931+cutebananaq@users.noreply.github.com>
2024-02-14 11:04:31 -0800

    Create CustomSong.rb
    a custom song that I am using alongside the challenges we do in class to learn about algorithm

---
+++ CustomSong.rb
@@ -0,0 +1,106 @@
+use_bpm 150
+use_synth :piano
+4.times do
+  play :fs5
+  play :cs5
+  play :a4
+  sleep 1
+  play :fs5
+  play :cs5
+  play :a4
+  sleep 1
+  play :fs5
+  play :cs5
+  play :a4
+  sleep 1
+end
+
+4.times do
+  play :e5
+  play :cs5
+  play :a4
+  sleep 1
+  play :e5
+  play :cs5
+  play :a4
+  sleep 1
+  play :e5
+  play :cs5
+  play :a4
+  sleep 1
+end
+
+4.times do
+  play :d4
+  play :fs4
+  play :a4
+  sleep 1
+  play :d4
+  play :fs4
+  play :a4
+  sleep 1
+  play :d4
+  play :fs4
+  play :a4
+  sleep 1
+end
+2.times do
+  play :cs4
+  play :fs4
+  play :g4
+  sleep 1
+  play :d4
+  play :fs4
+  play :a4
+  sleep 1
+  play :d4
+  play :fs4
+  play :a4
+  sleep 1
+end
+2.times do
+  play :cs4
+  play :f4
+  play :g4
+  sleep 1
+  play :d4
+  play :f4
+  play :a4
+  sleep 1
+  play :d4
+  play :f4
+  play :a4
+  sleep 1
+end
+4.times do
+  play :fs5
+  play :cs5
+  play :a4
+  sleep 1
+  play :fs5
+  play :cs5
+  play :a4
+  sample :drum_cymbal_soft, amp: 2
+  sample :drum_bass_hard, amp: 6
+  sleep 1
+  play :fs5
+  play :cs5
+  play :a4
+  sleep 1
+end
+
+
+4.times do
+  play :e5
+  play :cs5
+  play :a4
+  sleep 1
+  play :e5
+  play :cs5
+  play :a4
+  sleep 1
+  play :e5
+  play :cs5
+  play :a4
+  sleep 1
+end
